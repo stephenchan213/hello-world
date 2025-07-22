ce8e4b8420d76150686d4deec6935fb0eb423db8
stephenchan213 <cs_cty@yahoo.com>
2025-07-22 11:51:25 +0800

    Update ccass_scraper.py

--- ccass_scraper.py
+++ ccass_scraper.py
@@ -1,5 +1,5 @@
 import requests
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 # Define the paths for input and output files
 input_file_path = "stock_codes.txt"          # Relative path
