08f7f082d2fe3fa5487dc6503af88d1e92bf07f8
stephenchan213 <cs_cty@yahoo.com>
2025-07-22 11:36:07 +0800

    Add files via upload

---
+++ ccass_scraper.py
@@ -0,0 +1,34 @@
+import requests
+#from bs4 import BeautifulSoup
+
+# Define the paths for input and output files
+input_file_path = "C:/Users/chanSte/OneDrive - Computershare/Documents/stock_codes.txt"          # Relative path
+url = "https://www3.hkexnews.hk/sdw/search/searchsdw.aspx"
+
+# Read stock codes from the input file
+with open(input_file_path, 'r') as file:
+    stock_codes = file.read().splitlines()
+
+# Loop through each stock code and submit the request
+for stock_code in stock_codes:
+    payload = {
+        'today':'20250722',
+        'txStockCode': stock_code,
+        '__EVENTTARGET': 'btnSearch',
+        'txtShareholdingDate': '20250721',
+        'submit': 'Search'
+        # Add other parameters as needed
+    }
+    
+    # Send the POST request
+    response = requests.post(url, json=payload)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Open a text file in write mode
+        with open('response' + stock_code + '.txt', 'w') as file:
+            # Write the response body to the file
+            file.write(response.text)
+        print("Response body written to response" + stock_code + ".txt")
+    else:
+        print(f"Request failed with status code: {response.status_code}")
