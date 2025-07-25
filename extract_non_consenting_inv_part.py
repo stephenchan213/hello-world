b84cb139bf12620d6ee8ec8e6eb322e61c91b2c5
stephenchan213 <cs_cty@yahoo.com>
2025-07-25 10:36:50 +0800

    Update extract_non_consenting_inv_part.py

--- extract_non_consenting_inv_part.py
+++ extract_non_consenting_inv_part.py
@@ -42,7 +42,7 @@ def process_stock_code(stock_code):
                 headers_divs = datarow.find_all('div', class_='header')
                 values_divs = datarow.find_all('div', class_='value')
     
-                'data = [(stock_code, header.text.strip(), value.text.strip()) for header, value in zip(headers_divs, values_divs)]
+                #data = [(stock_code, header.text.strip(), value.text.strip()) for header, value in zip(headers_divs, values_divs)]
     
                 print(f"{header.text.strip()}: {value.text.strip()}") for header, value in zip(headers_divs, values_divs)]
                 
