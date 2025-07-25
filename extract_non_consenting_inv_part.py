e9373d01e83712760f4d6623da17bb349c6fc520
stephenchan213 <cs_cty@yahoo.com>
2025-07-25 12:01:43 +0800

    Update extract_non_consenting_inv_part.py

--- extract_non_consenting_inv_part.py
+++ extract_non_consenting_inv_part.py
@@ -73,7 +73,7 @@ for thread in threads:
 # Write results to CSV file
 with open('non_consenting_investors_all.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
-    table_headers = ['Stock Code'] + table_headers
+    table_headers.insert(0, 'Stock Code')
     writer.writerow(table_headers)
     writer.writerows(results)
 
